4dec084295c1efa1f0fc98f948d06bd8c5fd12b3
Johnny Black <john.p.black89@gmail.com>
2026-09-10 23:08:07 -0400

    Connection Updates

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
 // Set to the HTTPS address of your deployed API. Never put database credentials here.
-window.APP_CONFIG = { apiUrl: '' };
+window.APP_CONFIG = { apiUrl: "https://weareindecisive.onrender.com" };
